7cf7ea2c8ea83ad3fb15b82d841df8dcd69d1ad7
DaniloMendes0101 <danilomendes.e@gmail.com>
2017-10-10 12:40:21 -0300

    Add Auth

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>IBRC - Login</title>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="style_login.css">
+</head>
+<body>
+<div class="bg">
+    <div class="logo"></div>
+    <div class="login-block">
+        <h1>Login</h1>
+        <form action="">
+            <input type="text" placeholder="Usuário" id="username" required>
+            <input type="password" placeholder="Senha" id="password" required>
+        </form>
+        <button>Conectar</button>
+    </div>
+</div>
+</body>
+</html>
