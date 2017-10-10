fbc23cadbb1f504f6cc7d4b5a235283b6dd7086c
DaniloMendes0101 <danilomendes.e@gmail.com>
2017-10-10 14:21:51 -0300

    Add CDN Bootstrap css

--- Modules/Login/Http/routes.php
+++ Modules/Login/Http/routes.php
@@ -3,4 +3,7 @@
 Route::group(['middleware' => 'web', 'prefix' => 'login', 'namespace' => 'Modules\Login\Http\Controllers'], function()
 {
     Route::get('/view', 'LoginController@view');
+
+
+    Route::get('/logar', 'LoginController@postLogar')->name('login/logar');
 });
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>IBRC - Login</title>
     <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style_login.css">
 </head>
 <body>
@@ -10,7 +11,7 @@
     <div class="logo"></div>
     <div class="login-block">
         <h1>Login</h1>
-        <form action="{{route('login/')}}">
+        <form action="{{route('login/logar')}}">
             <input type="text" placeholder="Usuário" id="username" required>
             <input type="password" placeholder="Senha" id="password" required>
         </form>
